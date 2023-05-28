cc934869a961b0fb9abced5f95870ce81de4d601
Jose Rafael García <jg1238610@gmail.com>
2023-05-28 11:20:39 -0600

    First Commit

---
+++ src/main/java/org/jrgf/chip8/emu/ChipPanel.java
@@ -0,0 +1,31 @@
+package org.jrgf.chip8.emu;
+
+import org.jrgf.chip8.emu.chip.Chip;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ChipPanel extends JPanel{
+
+
+        private Chip chip;
+
+        public ChipPanel(Chip chip) {
+            this.chip = chip;
+        }
+
+        public void paint(Graphics g) {
+            byte[] display = chip.getDisplay();
+            for(int i = 0; i < display.length; i++) {
+                if(display[i] == 0)
+                    g.setColor(Color.BLACK);
+                else
+                    g.setColor(Color.WHITE);
+
+                int x = (i % 64);
+                int y = (int)Math.floor(i / 64);
+
+                g.fillRect(x * 10, y * 10, 10, 10);
+            }
+        }
+}
